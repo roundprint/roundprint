e4cf83d0e0abee1d4fb42fa9abac0a386367716b
Advance Ngwarati <advance.d.ngwarati@gmail.com>
2019-05-09 10:31:30 +0200

    Setting Server Enviroment

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+///////////////////////////////////////
+//          INITILIAZE APP          //
+/////////////////////////////////////
+
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+  
+const port = process.env.PORT || 5001;
+
+app.listen(port,()=>{
+    console.log(`Server is running at port ${port}`);
+});
