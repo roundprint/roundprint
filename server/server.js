93fda63aedaf02af0a04491291b1468488a3ae8b
Advance Ngwarati <advance.d.ngwarati@gmail.com>
2019-06-25 09:05:06 +0200

    Routing

--- server/server.js
+++ server/server.js
@@ -39,7 +39,11 @@ app.use(bodyParser.json());
 //         SERVE UP STATIC ASSEST
 //=================================
 if(process.env.MODE_ENV === 'production'){
-  app.use(express.static("client/build"));
+  app.use(express.static(path.resolve(__dirname+"/../client/build")));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"../client/build/index.html"));
+  });
 }
 
 //=================================
